3f47f4f19df7f94bebaaf78c697b089f7cea0af6
sislam5 <sislam5@ivytech.edu>
2026-04-11 20:45:30 -0500

    Delete Mo3_assignment_list_7.5.py

--- Mo3_assignment_list_7.5.py
+++
@@ -1,5 +0,0 @@
-things = ["mozzarella", "cinderella", "salmonella"]
-# Make 'mozzarella' uppercase
-things[0] = things[0].upper()
-print(things)
-# Output: ['MOZZARELLA', 'Cinderella', 'salmonella']
