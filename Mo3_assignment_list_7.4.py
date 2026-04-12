43c966b590273af80077775e66f0881180b2144c
sislam5 <sislam5@ivytech.edu>
2026-04-11 20:45:18 -0500

    Delete Mo3_assignment_list_7.4.py

--- Mo3_assignment_list_7.4.py
+++
@@ -1,5 +0,0 @@
-things = ["mozzarella", "cinderella", "salmonella"]
-# Capitalize 'cinderella'
-things[1] = things[1].capitalize()
-print(things)
-# Output: ['mozzarella', 'Cinderella', 'salmonella']
